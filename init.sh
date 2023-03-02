722a71e0186cbc668474e7620867f2ca43ec5e42
Auciomar Cerqueira <auciomar.cerqueira@serpro.gov.br>
2023-03-02 18:48:13 -0300

    update

--- init.sh
+++ init.sh
@@ -2,8 +2,11 @@
 
 cd ..
 
-gh repo clone wquote/frontend
+git clone git@github.com:wquote/frontend.git 
+# gh repo clone wquote/frontend
 
-gh repo clone wquote/database
+git clone git@github.com:wquote/backend.git 
+# gh repo clone wquote/backend
 
-gh repo clone wquote/backend
+git clone git@github.com:wquote/database.git 
+# gh repo clone wquote/database
